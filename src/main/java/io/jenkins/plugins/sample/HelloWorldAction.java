640531f73d472f047e30ad668b14bc0b8d9667bf
Jason Wee <peichieh@gmail.com>
2019-04-02 15:23:32 +0800

    no spaces

--- src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -6,54 +6,54 @@ import jenkins.model.RunAction2;
 
 
 public class HelloWorldAction implements RunAction2 {
-	
+    
     private transient Run run;
-	
-	private String name;
-	
-	public HelloWorldAction(String name) {
-		this.name = name;
-	}
-	
+    
+    private String name;
+    
+    public HelloWorldAction(String name) {
+        this.name = name;
+    }
+    
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	@Override
-	public String getIconFileName() {
-		return "document.png";
-	}
-
-	@Override
-	public String getDisplayName() {
-		return "Greeting";
-	}
-
-	@Override
-	public String getUrlName() {
-		return "greeting";
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
-	@Override
-	public void onAttached(Run<?, ?> run) {
-		this.run = run;
-	}
+    @Override
+    public String getIconFileName() {
+        return "document.png";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Greeting";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "greeting";
+    }
 
 
-	@Override
-	public void onLoad(Run<?, ?> run) {
-		this.run = run;
-	}
+    @Override
+    public void onAttached(Run<?, ?> run) {
+        this.run = run;
+    }
 
-	public Run getRun() {
-		return run;
-	}
+
+    @Override
+    public void onLoad(Run<?, ?> run) {
+        this.run = run;
+    }
+
+    public Run getRun() {
+        return run;
+    }
 
 }
